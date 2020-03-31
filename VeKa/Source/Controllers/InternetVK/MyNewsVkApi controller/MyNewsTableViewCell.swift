f29796169ced68e34454d55ec54fc72522889cd1
slipStas <41761920+slipStas@users.noreply.github.com>
2020-03-31 15:50:38 +0300

    add news controller

---
+++ VeKa/Source/Controllers/InternetVK/MyNewsVkApi controller/MyNewsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewsTableViewCell.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 31.03.2020.
+//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class MyNewsTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var avatarOwnerNews: UIImageView!
+    @IBOutlet weak var nameOwnerNewsImage: UILabel!
+    @IBOutlet weak var textOfNews: UITextView!
+}
---
+++ VeKa/Source/Controllers/InternetVK/MyNewsVkApi controller/MyNewsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MyNewsViewController.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 31.03.2020.
+//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class MyNewsViewController: UIViewController {
+
+    @IBOutlet weak var myNewsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myNewsTableView.dataSource = self
+    }
+    
+}
+
+extension MyNewsViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myNewsTableView.dequeueReusableCell(withIdentifier: "myNewsTable", for: indexPath) as! MyNewsTableViewCell
+        
+        cell.avatarOwnerNews.layer.masksToBounds = true
+        cell.avatarOwnerNews.layer.cornerRadius = cell.avatarOwnerNews.frame.height / 2
+        
+        
+        cell.avatarOwnerNews.image = #imageLiteral(resourceName: "icon_1")
+        cell.nameOwnerNewsImage.text = "owner news"
+        cell.textOfNews.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+        
+        return cell
+    }
+    
+    
+}
