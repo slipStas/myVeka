bf8cd21acd56419df04d04fb27a731579dff783a
slipStas <41761920+slipStas@users.noreply.github.com>
2020-04-03 16:57:18 +0300

    change getting token request "scope"

--- VeKa/Source/Controllers/InternetVK/AuthoriseViewController.swift
+++ VeKa/Source/Controllers/InternetVK/AuthoriseViewController.swift
@@ -28,7 +28,7 @@ class AuthoriseViewController: UIViewController {
             URLQueryItem(name: "client_id", value: "7332528"),
             URLQueryItem(name: "display", value: "mobile"),
             URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
-            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "scope", value: "270342"),
             URLQueryItem(name: "response_type", value: "token"),
             URLQueryItem(name: "v", value: "5.68")
         ]
---
+++ VeKa/Source/Model/VkApi models/MyNews VKApi/GetNewsVkApi.swift
@@ -0,0 +1,65 @@
+//
+//  GetNewsVkApi.swift
+//  VeKa
+//
+//  Created by Stanislav Slipchenko on 03.04.2020.
+//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftKeychainWrapper
+
+class GetNewsVkApi {
+    
+    var getGroupsVkApi : GroupsVkAPI?
+    
+    let userId = KeychainWrapper.standard.string(forKey: Session.Keys.hardUserId.rawValue) ?? ""
+    let token = KeychainWrapper.standard.string(forKey: Session.Keys.hardToken.rawValue) ?? ""
+    let url = "https://api.vk.com.method/"
+    
+    func getGroups(completionHandler: @escaping(Bool) -> ()) {
+        
+        
+        let accessParameters: Parameters = ["access_token" : token, "user_id" : userId]
+
+        var urlGroups = URLComponents()
+        urlGroups.scheme = "https"
+        urlGroups.host = "api.vk.com"
+        urlGroups.path = "/method/newsfeed.get"
+        urlGroups.queryItems = [
+            URLQueryItem(name: "filters", value: "post"),
+            URLQueryItem(name: "v", value: "5.102")
+        ]
+        
+        guard let url = urlGroups.url else {return}
+        
+        AF.request(url, parameters: accessParameters).responseData { data in
+            guard let data = data.value else {
+                completionHandler(false)
+                return }
+           
+            let groups = try! JSONDecoder().decode(GroupsVkAPI.self, from: data)
+            self.getGroupsVkApi = groups
+            let items = groups.response.items
+            for i in 0..<items.count {
+                
+                let groups = GroupRealm()
+                
+                groups.name = items[i].name
+                groups.id = items[i].id
+                groups.photo = items[i].photo50
+                
+                do {
+                    try Session.shared.realm.write {
+                        Session.shared.realm.add(groups, update: .all)
+                    }
+                } catch {
+                    completionHandler(false)
+                    print("error")
+                }
+            }
+           completionHandler(true)
+        }
+    }
+}
