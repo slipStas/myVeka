5da84793dc699f398ae6e2c88dbec778bc54f69d
slipStas <41761920+slipStas@users.noreply.github.com>
2020-04-07 14:35:16 +0300

    change mapping JSON (friends, groups)

--- VeKa/Source/Model/VkApi models/Friends VKApi/UserVkAPI.swift
+++ VeKa/Source/Model/VkApi models/Friends VKApi/UserVkAPI.swift
@@ -16,11 +16,11 @@ class UserVkAPI: Codable {
     }
     // MARK: - Response
     class Response: Codable {
-        let count: Int
+        //let count: Int
         let items: [Item]
 
-        init(count: Int, items: [Item]) {
-            self.count = count
+        init(items: [Item]) {
+            //self.count = count
             self.items = items
         }
     }
@@ -29,36 +29,36 @@ class UserVkAPI: Codable {
     class Item: Codable {
         let id: Int
         let firstName, lastName: String
-        let isClosed, canAccessClosed: Bool?
+        //let isClosed, canAccessClosed: Bool?
         let photo50: String
-        let online: Int
-        let trackCode: String
-        let lists: [Int]?
-        let deactivated: String?
+//        let online: Int
+//        let trackCode: String
+//        let lists: [Int]?
+//        let deactivated: String?
 
         enum CodingKeys: String, CodingKey {
             case id
             case firstName = "first_name"
             case lastName = "last_name"
-            case isClosed = "is_closed"
-            case canAccessClosed = "can_access_closed"
+//            case isClosed = "is_closed"
+//            case canAccessClosed = "can_access_closed"
             case photo50 = "photo_50"
-            case online
-            case trackCode = "track_code"
-            case lists, deactivated
+//            case online
+//            case trackCode = "track_code"
+//            case lists, deactivated
         }
 
-        init(id: Int, firstName: String, lastName: String, isClosed: Bool?, canAccessClosed: Bool?, photo50: String, online: Int, trackCode: String, lists: [Int]?, deactivated: String?) {
+        init(id: Int, firstName: String, lastName: String/*, isClosed: Bool?, canAccessClosed: Bool?*/, photo50: String/*, online: Int, trackCode: String, lists: [Int]?, deactivated: String?*/) {
             self.id = id
             self.firstName = firstName
             self.lastName = lastName
-            self.isClosed = isClosed
-            self.canAccessClosed = canAccessClosed
+//            self.isClosed = isClosed
+//            self.canAccessClosed = canAccessClosed
             self.photo50 = photo50
-            self.online = online
-            self.trackCode = trackCode
-            self.lists = lists
-            self.deactivated = deactivated
+//            self.online = online
+//            self.trackCode = trackCode
+//            self.lists = lists
+//            self.deactivated = deactivated
         }
     }
 }
