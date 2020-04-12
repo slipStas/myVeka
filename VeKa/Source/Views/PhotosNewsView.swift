660bd4330e0b502387d8e6bbe3bf7576467f4788
slipStas <41761920+slipStas@users.noreply.github.com>
2020-04-12 12:59:58 +0300

    111

--- VeKa/Source/Views/PhotosNewsView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PhotosNewsView.swift
-//  VeKa
-//
-//  Created by Stanislav Slipchenko on 11.04.2020.
-//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
-//
-
-import Foundation
